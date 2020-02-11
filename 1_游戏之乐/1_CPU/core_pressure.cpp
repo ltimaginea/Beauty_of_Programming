9e8e8c7958fd6f5db74f523d01f7ee227d7e2c2a
ltimaginea <ltimaginea@163.com>
2020-02-11 23:29:33 +0800

    core pressure

---
+++ 1_游戏之乐/1_CPU/core_pressure.cpp
@@ -0,0 +1,33 @@
+﻿void addMem(int cnt)
+{
+	int * ptr = NULL;
+	int some_memory = 1024 * 1024 * (cnt - 1);
+	ptr = (int *)malloc(some_memory);
+	if (ptr != NULL)
+	{
+		memset(ptr, 0, some_memory);
+		printf("malloc is OK!\n");
+	}
+	else
+	{
+		printf("malloc is false!\n");
+	}
+	pause();
+	//usleep(time);
+}
+
+void addCPU(int cnt)
+{
+	int period = 500;
+	int busyTime = (cnt - 1)*period / 100;
+	int idleTime = 100 * (period - busyTime);
+	while (true)
+	{
+		clock_t t = clock();
+		while ((((double)(clock() - t)) / CLOCK_PER_SEC * 1000) <= busyTime)
+		{
+			;
+		}
+		usleep(idleTime);
+	}
+}
