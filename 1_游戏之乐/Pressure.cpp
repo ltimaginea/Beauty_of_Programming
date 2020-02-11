2d6d31704ccd96f783d5a1527e7697790fd9b205
ltimaginea <ltimaginea@163.com>
2020-02-11 22:43:15 +0800

    Pressure

---
+++ 1_游戏之乐/Pressure.cpp
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <unistd.h>
+
+struct
+{
+	char tmp[100];
+	int count_tmp;
+}name, vh, pp, mm;
+
+void usage(FILE* fp, int rc)
+{
+	fprintf(fp, "Usage: ./pressure [-h?v] [-m <count>] [-p <count>] [-t <time>] \n\n");
+	fprintf(fp, "        -h?            this help\n");
+	fprintf(fp, "        -v             print version info\n");
+	fprintf(fp, "        -m count       used MB of memory\n");
+	fprintf(fp, "        -p count       cpu occupancy num\n");
+	fprintf(fp, "        -t time        run some seconds\n");
+	exit(rc);
+}
+
+//
+int main(int argc, char* argv[])
+{
+	return 0;
+}
